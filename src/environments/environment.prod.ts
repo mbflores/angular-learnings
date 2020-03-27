02afc1267bd81b1360ffa5c94c75386708a31b09
Mel Bryan Flores <melbryanflores316@hotmail.com>
2020-03-27 22:03:15 +0800

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB5s3z39HB-ZXNA6vLICyJQTIsQVB2zkrc',
+    authDomain: 'oshop-ab7e0.firebaseapp.com',
+    databaseURL: 'https://oshop-ab7e0.firebaseio.com',
+    projectId: 'oshop-ab7e0',
+    storageBucket: 'oshop-ab7e0.appspot.com',
+    messagingSenderId: '243158768512',
+    appId: '1:243158768512:web:cd1278d9b4f5b8f7e97f65',
+    measurementId: 'G-4LJPN0HFRT'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB5s3z39HB-ZXNA6vLICyJQTIsQVB2zkrc',
+    authDomain: 'oshop-ab7e0.firebaseapp.com',
+    databaseURL: 'https://oshop-ab7e0.firebaseio.com',
+    projectId: 'oshop-ab7e0',
+    storageBucket: 'oshop-ab7e0.appspot.com',
+    messagingSenderId: '243158768512',
+    appId: '1:243158768512:web:cd1278d9b4f5b8f7e97f65',
+    measurementId: 'G-4LJPN0HFRT'
+  }
 };
 
 /*
